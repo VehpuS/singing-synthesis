a182c1c7c756bab8cfe38301d2bb07c65aff0501
Moshe Gordon Radian <vehpus@gmail.com>
2024-03-02 22:01:42 +0200

    feat: audio generation works on front end! currently prompted by user, automation is next :)

--- musicxml-singer-with-oddvoices/src/oddvoices/cpp/oddvoices_wasm.cpp
+++ musicxml-singer-with-oddvoices/src/oddvoices/cpp/oddvoices_wasm.cpp
@@ -2,9 +2,26 @@
 #include <emscripten/bind.h>
 #include "sing.hpp"
 
-std::string sing(
-    oddvoices::Voice &voice, oddvoices::g2p::G2P &g2p, std::string json, std::string outWAVFile, std::string lyrics)
+// Create an enum for the voices
+enum Voice
 {
+    AIR = 0,
+    CICADA = 1,
+    QUAKE = 2
+};
+
+std::string sing(int voiceIndex, std::string json, std::string outWAVFile, std::string lyrics)
+{
+
+    // Setup the G2P Class
+    oddvoices::g2p::G2P g2p("oddvoices/cmudict-0.7b");
+
+    // Setup the Voice Class
+    std::string voicePath = voiceIndex == AIR ? "voices/air.voice" : voiceIndex == CICADA ? "voices/cicada.voice" : "voices/quake.voice";
+    oddvoices::Voice voice;
+    voice.initFromFile(voicePath);
+
+    // Sing the JSON
     auto [ok, error] = oddvoices::frontend::singJSON(
         voice, g2p, json, outWAVFile, lyrics);
     if (ok)
@@ -17,9 +34,4 @@ std::string sing(
 EMSCRIPTEN_BINDINGS(oddvoices_wasm)
 {
     emscripten::function("sing", &sing);
-    emscripten::class_<oddvoices::Voice>("Voice")
-        .constructor<>()
-        .function("initFromFile", &oddvoices::Voice::initFromFile);
-    emscripten::class_<oddvoices::g2p::G2P>("G2P")
-        .constructor<std::string>();
 }
