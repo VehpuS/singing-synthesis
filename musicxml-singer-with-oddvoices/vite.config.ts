96ae1135a21f3e3e607a01428072bfdc79748e64
Moshe Gordon Radian <vehpus@gmail.com>
2024-03-07 01:13:48 +0200

    fix: attempt to reduce build mangling to get the wasm code to work

--- musicxml-singer-with-oddvoices/vite.config.ts
+++ musicxml-singer-with-oddvoices/vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/singing-synthesis",
+  plugins: [react()],
+  base: "/singing-synthesis",
+  optimizeDeps: {
+    noDiscovery: true,
+  },
+  build: {
+    minify: false,
+    terserOptions: {
+      keep_classnames: true,
+      keep_fnames: true,
+      compress: false,
+      mangle: false,
+      format: {
+        preserve_annotations: true,
+      },
+    },
+  },
 });
