21ce26d94b68eb665cfc996a4e0d284bf51b6cb8
Moshe Gordon Radian <vehpus@gmail.com>
2024-03-07 02:07:56 +0200

    fix: restore vite config, add dynamic title, local build preview works with simple file server

--- musicxml-singer-with-oddvoices/vite.config.ts
+++ musicxml-singer-with-oddvoices/vite.config.ts
@@ -5,23 +5,4 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/singing-synthesis",
-  optimizeDeps: {
-    noDiscovery: true,
-  },
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      treeshake: false,
-    },
-    minify: false,
-    terserOptions: {
-      keep_classnames: true,
-      keep_fnames: true,
-      compress: false,
-      mangle: false,
-      format: {
-        preserve_annotations: true,
-      },
-    },
-  },
 });
